f1496b4e5e68ed221758774a9a25dc26e9048f4d
Lucie <lucie.stenger@gmail.com>
2024-10-15 15:37:14 +0200

    updating flask app and SQL files

--- notebooks/Telecoms_deals.sql
+++ notebooks/Telecoms_deals.sql
@@ -77,6 +77,8 @@ SET SQL_SAFE_UPDATES = 1;
 SET SQL_SAFE_UPDATES = 0;
 ALTER TABLE tmobile_plans ADD COLUMN Cleaned_Price_Per_Line DECIMAL(10, 2);
 ALTER TABLE tmobile_plans ADD COLUMN Cleaned_Total_Monthly_Price DECIMAL(10, 2);
+ALTER TABLE tmobile_plans ADD COLUMN Cleaned_Line_Number DECIMAL(10);
+
 
 UPDATE tmobile_plans
 SET Cleaned_Price_Per_Line = 
@@ -84,6 +86,9 @@ TRIM(REPLACE(REPLACE(REPLACE(REPLACE(Price_Per_Line, '$', ''),'/line',''),'line'
 UPDATE tmobile_plans
 SET Cleaned_Total_Monthly_Price = 
 TRIM(REPLACE(REPLACE(Total_Monthly_Price, '$', ''),'/month','')) WHERE Total_Monthly_Price IS NOT NULL;
+UPDATE tmobile_plans
+SET Cleaned_Line_Number = 
+TRIM(REPLACE(Line_Number, 'lines', '')) WHERE Line_Number IS NOT NULL;
 SET SQL_SAFE_UPDATES = 1;
 
 -- Identifying internet providers with the most plans to help consumers identify the most flexible options.
@@ -101,6 +106,86 @@ ON ip.Carrier = bp.Carrier
 GROUP BY ip.Carrier 
 ORDER BY Best_Plan_Count DESC;
 
--- number of customers with a partner + dependent wish to have family plan
--- within these customers are they using internet or telephone or both
--- could group if partner or dependent = yes then look at internet use and provide prices
+-- Churn rate for customers with bundle (phone service + internet service) option
+SELECT 
+    (SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*)) AS Churn_Rate
+FROM 
+    telco_df_for_sql
+WHERE 
+    PhoneService = 'Yes'
+    AND InternetService != 'No';
+-- Churn rate for customers with only internet service option
+SELECT 
+    (SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*)) AS Churn_Rate
+FROM 
+    telco_df_for_sql
+WHERE 
+    PhoneService = 'No'
+    AND InternetService != 'No';
+-- Churn rate for customers with only phone service option
+SELECT 
+    (SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*)) AS Churn_Rate
+FROM 
+    telco_df_for_sql
+WHERE 
+    PhoneService = 'Yes'
+    AND InternetService = 'No';
+    
+-- In our synthetic telco customer data, the churn rate for customers with bundle services is higher than for phone service alone or for internet service alone.
+-- This might be because customers will spend more with specific provider, and therefore save more when switching to a cheaper provider.
+SELECT AVG(bp.Cleaned_Price) AS Bundle_Average_Price, AVG(cp.Cleaned_Price) AS Cellphone_Average_Price
+FROM bundled_plans AS bp, cell_phone_plans AS cp;
+
+SELECT 
+    AVG(CASE WHEN PhoneService = 'Yes' AND InternetService = 'No' THEN MonthlyCharges 
+            ELSE NULL END) AS Average_Price_phone,
+    AVG(CASE WHEN PhoneService = 'No' AND InternetService != 'No' THEN MonthlyCharges 
+            ELSE NULL END) AS Average_Price_internet,
+	AVG(CASE WHEN PhoneService = 'Yes' AND InternetService != 'No' THEN MonthlyCharges 
+            ELSE NULL END) AS Average_Price_bundle
+FROM 
+    telco_df_for_sql;
+
+-- In our synthetic telco customer data, the bundle price average is higher than the sum of average phone service price and average internet service price.
+-- This could mean that in our telco company, there is no bundle offer in place (or only a very small price reduction) and this would explain why there is a higher chance of churn for the customers using both internet and phone services, as most competitors have bundles in place.
+
+
+-- Churn rate for customers with multiple phone lines
+SELECT 
+    (SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*)) AS Churn_Rate
+FROM 
+    telco_df_for_sql
+WHERE 
+    MultipleLines = 'Yes';
+-- Churn rate for customers with only one phone line
+SELECT 
+    (SUM(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*)) AS Churn_Rate
+FROM 
+    telco_df_for_sql
+WHERE 
+    MultipleLines = 'No';
+    
+-- In our synthetic telco customer data, the churn rate for customers subscribing to multiple phone lines is slightly higher than for customers subscribing to only one phone line.
+-- When looking at T-Mobile family plans, offering savings on multiple lines, it appears clear that this is also a way to reduce churn rate.
+-- It seems even more relevant when analyzing the total cost for 2 lines versus 3 lines in various phone plans.
+
+SELECT 
+    Plan,
+    Line_Number,
+    Cleaned_Total_Monthly_Price,
+    Cleaned_Total_Monthly_Price - LAG(Cleaned_Total_Monthly_Price) OVER (PARTITION BY Plan ORDER BY Cleaned_Line_Number) 
+    AS Price_Difference_Per_New_Line
+FROM 
+    tmobile_plans
+ORDER BY 
+    Plan, 
+    Line_Number;
+
+-- We can see that for both entry-level family plans (essentials and Go5G) there is no price difference in total spent between 2 lines and 3 lines.
+-- We can interpret this as the following: customers churn more when adding a third line to their subscription, probably looking at competitors before making this choice.
+-- If we assume that the 2 lines first lines of the family plan are usually subscribed by the parents, and the third line would be when they wish to add a kid,
+-- they would spend time making sure their current phone carrier is appropriate for everyone, in terms of price and in terms of capabilities.
+-- By keeping the exact same price for 2 or 3 lines for some of the plans, carriers are making it easy for parents to stay with the same carrier, and one they added one kid to their family plan, they would probably add the others too.
+
+
+
